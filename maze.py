1c0e5a620f455505cfdb406fe44ad949eb6d7e2b
Joe Hsiao <joe.phsiao@gmail.com>
2019-07-16 18:06:43 -0400

    Refactor __str__() so it is more clear.

--- maze.py
+++ maze.py
@@ -12,7 +12,7 @@ class Maze:
         self.R = r
         self.C = c
         self.start = (0, 0)
-        self.goal = (r-1, c-1)
+        self.goal = (r - 1, c - 1)
 
         # each cell has 4 walls: left, right, up, down
         # cell: 1 -> not visited. 0 -> visited
@@ -37,8 +37,9 @@ class Maze:
                 new_r, new_c = r + offsets[d][0], c + offsets[d][1]
                 if 0 <= new_r < self.R and 0 <= new_c < self.C and self.cells[new_r][new_c] == 1:
                     self.walls[r][c][d] = 1
-                    self.walls[new_r][new_c][d+1 if d%2 == 0 else d-1] = 1
+                    self.walls[new_r][new_c][d + 1 if d % 2 == 0 else d - 1] = 1
                     dfs(new_r, new_c)
+
         dfs(*self.goal)
 
     def __str__(self):
@@ -50,28 +51,34 @@ class Maze:
         #     * * * * *
         #     *   *   *
         #     * * * * *
-        mat = [' '.join([self.wall_sym for _ in range(self.C*3-self.C+1)])]
+        def get_cell(r, c):
+            ret = [' ', ' ', ' ']  # space x 3
+            if r == self.start[0] and c == self.start[1]:
+                ret[1] = self.start_sym
+            elif r == self.goal[0] and c == self.goal[1]:
+                ret[1] = self.goal_sym
+            return ''.join(ret)
+
+        def get_right_wall(r, c):
+            return ' ' if self.walls[r][c][1] == 1 else self.wall_sym
+
+        def get_down_wall(r, c):
+            empty = '   ' + self.wall_sym
+            block = self.wall_sym * 4
+            return empty if self.walls[r][c][3] == 1 else block
+
+        mat = [' '.join([self.wall_sym for _ in range(self.C * 3 - self.C + 1)])]
         for r in range(self.R):
             lr_path, ud_path = self.wall_sym, self.wall_sym
             for c in range(self.C):
-                # draw cell
-                if r == self.start[0] and c == self.start[1]:
-                    lr_path += ' ' + self.start_sym + ' '
-                elif r == self.goal[0] and c == self.goal[1]:
-                    lr_path += ' ' + self.goal_sym + ' '
-                else:
-                    lr_path += '   '  # space x 3
-                
-                # draw right path
-                if self.walls[r][c][1] == 1: lr_path += ' '
-                else:
-                    lr_path += self.wall_sym
-                
-                # draw down path
-                if self.walls[r][c][3] == 1:
-                    ud_path += '   ' + self.wall_sym
-                else:
-                    ud_path += ' ' + self.wall_sym + ' ' + self.wall_sym
+                lr_path += get_cell(r, c)
+                lr_path += get_right_wall(r, c)
+                ud_path += get_down_wall(r, c)
             mat.append(lr_path)
             mat.append(ud_path)
         return '\n'.join(mat)
+
+
+if __name__ == "__main__":
+    m = Maze()
+    print(m)
